655caab4f5f1153f2b606958792ba3bedd6dafbb
xzonzh@gmail.com <xzonzh@gmail.com@d229cd2e-d496-f8b7-c295-5a573b23ac90>
2011-08-17 01:14:45 +0000

    DebugUtil,TimespendDebug

--- trunk/org.hxzon/org/hxzon/util/DebugUtil.java
+++ trunk/org.hxzon/org/hxzon/util/DebugUtil.java
@@ -1,12 +1,23 @@
 package org.hxzon.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DebugUtil {
+	public static boolean open = false;
 	public static final int error = 1;
 	public static final int info = 2;
 	public static final int debug = 3;
 	public static final int trace = 4;
+	private static final List<String> classNames = new ArrayList<String>();
+	static {
+
+	}
 
 	private static void output(int type, String className, String methodName, int lineNumber, String message) {
+		if (!classNames.contains(className)) {
+			return;
+		}
 		String level = "";
 		switch (type) {
 		case 1:
@@ -23,7 +34,7 @@ public class DebugUtil {
 			break;
 		default:
 		}
-		className = className.substring(className.lastIndexOf('.')+1);
+		className = className.substring(className.lastIndexOf('.') + 1);
 		String output = (level + ":" + className + "." + methodName + "(" + lineNumber + ")" + ":" + message);
 		if (type == error) {
 			System.err.println(output);
@@ -32,12 +43,18 @@ public class DebugUtil {
 		}
 	}
 
+	private static void output(int type, String message) {
+		if (open) {
+			StackTraceElement ste = Thread.currentThread().getStackTrace()[3];
+			String className = ste.getClassName();
+			String methodName = ste.getMethodName();
+			int lineNumber = ste.getLineNumber();
+			output(type, className, methodName, lineNumber, message);
+		}
+	}
+
 	public static void error(String message) {
-		StackTraceElement ste = Thread.currentThread().getStackTrace()[2];
-		String className = ste.getClassName();
-		String methodName = ste.getMethodName();
-		int lineNumber = ste.getLineNumber();
-		output(error, className, methodName, lineNumber, message);
+		output(error, message);
 	}
 
 	public static void error(String message, Throwable e) {
@@ -45,27 +62,15 @@ public class DebugUtil {
 	}
 
 	public static void debug(String message) {
-		StackTraceElement ste = Thread.currentThread().getStackTrace()[2];
-		String className = ste.getClassName();
-		String methodName = ste.getMethodName();
-		int lineNumber = ste.getLineNumber();
-		output(debug, className, methodName, lineNumber, message);
+		output(debug, message);
 	}
 
 	public static void trace(String message) {
-		StackTraceElement ste = Thread.currentThread().getStackTrace()[2];
-		String className = ste.getClassName();
-		String methodName = ste.getMethodName();
-		int lineNumber = ste.getLineNumber();
-		output(trace, className, methodName, lineNumber, message);
+		output(trace, message);
 	}
 
 	public static void info(String message) {
-		StackTraceElement ste = Thread.currentThread().getStackTrace()[2];
-		String className = ste.getClassName();
-		String methodName = ste.getMethodName();
-		int lineNumber = ste.getLineNumber();
-		output(info, className, methodName, lineNumber, message);
+		output(info, message);
 	}
 
 }
