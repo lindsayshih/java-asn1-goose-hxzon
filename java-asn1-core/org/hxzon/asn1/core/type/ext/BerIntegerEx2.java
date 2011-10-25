81d2c83d79af836e85118cd784dbd4615045575d
xzonzh <xzonzh@gmail.com>
2011-10-25 04:21:18 +0000

    BerIntegerEx,BerIntegerEx2

--- java-asn1-core/org/hxzon/asn1/core/type/ext/BerIntegerEx2.java
+++ java-asn1-core/org/hxzon/asn1/core/type/ext/BerIntegerEx2.java
@@ -8,34 +8,39 @@ import org.hxzon.asn1.core.type.BerInteger;
 
 public class BerIntegerEx2 extends BerInteger {
 
-    private final Map<String, String> valueStrings = new HashMap<String, String>();
-    private final Map<String, String> displayStrings = new HashMap<String, String>();
+    private static final Map<String, String> valueStrings = new HashMap<String, String>();
+    private static final Map<String, String> displayStrings = new HashMap<String, String>();
 
     public String getValueAsString() {
         int tagNumber = getTag() & Tag.TAGNUMBER_MASK;
-        String valueAsString = valueStrings.get(tagNumber + "-" + getValue());
+        String prefix = getClass().getName() + "-" + tagNumber;
+        String valueAsString = valueStrings.get(prefix + "-" + getValue());
         if (valueAsString != null) {
             return valueAsString;
         } else {
-            valueAsString = valueStrings.get(tagNumber + "-x");
+            valueAsString = valueStrings.get(prefix + "-x");
             return valueAsString != null ? valueAsString : super.getValueAsString();
         }
     }
 
-    public void addValueString(int tagNumber, int value, String valueAsString) {
-        valueStrings.put(tagNumber + "-" + value, valueAsString);
+    public static void addValueString(int tagNumber, int value, String valueAsString, Class<?> clazz) {
+        String prefix = clazz.getName() + "-" + tagNumber;
+        valueStrings.put(prefix + "-" + value, valueAsString);
     }
 
-    public void addValueString(int tagNumber, String valueAsString) {
-        valueStrings.put(tagNumber + "-x", valueAsString);
+    public static void addValueString(int tagNumber, String valueAsString, Class<?> clazz) {
+        String prefix = clazz.getName() + "-" + tagNumber;
+        valueStrings.put(prefix + "-x", valueAsString);
     }
 
-    public void addDisplayString(int tagNumber, String displayString) {
-        displayStrings.put(String.valueOf(tagNumber), displayString);
+    public static void addDisplayString(int tagNumber, String displayString, Class<?> clazz) {
+        String prefix = clazz.getName() + "-" + tagNumber;
+        displayStrings.put(prefix, displayString);
     }
 
     public String getDisplayString() {
         int tagNumber = getTag() & Tag.TAGNUMBER_MASK;
-        return displayStrings.get(String.valueOf(tagNumber));
+        String prefix = getClass().getName() + "-" + tagNumber;
+        return displayStrings.get(prefix);
     }
 }
