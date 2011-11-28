189a9d0951d2c81fd7c1f4a7817cef8b610bf694
xzonzh <xzonzh@gmail.com>
2011-11-28 04:04:05 +0000

    org.hxzon.swing.easy.components.table.EasyTable

--- org.hxzon/org/hxzon/swing/easy/components/table/EasyTableColumn.java
+++ org.hxzon/org/hxzon/swing/easy/components/table/EasyTableColumn.java
@@ -1,11 +1,13 @@
 package org.hxzon.swing.easy.components.table;
 
+import javax.swing.event.CellEditorListener;
+import javax.swing.event.ChangeEvent;
 import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
-public class EasyTableColumn extends TableColumn{
-//   protected int       modelIndex;
+public class EasyTableColumn<V> extends TableColumn implements CellEditorListener {
+    private static final long serialVersionUID = 1L;
+    //   protected int       modelIndex;
 //
 //   protected Object    identifier;
 //
@@ -26,7 +28,66 @@ public class EasyTableColumn extends TableColumn{
 //   protected TableCellEditor   cellEditor;
 //
 //   protected boolean   isResizable;
-   //
+    //
     private boolean isEditable;
+
+    private String columnName;
     
+    private Class valueClass;
+
+    public EasyTableColumn() {
+        super();
+    }
+
+    public Object getValue(EasyTable<V> table, V rowValue) {
+        return null;
+    }
+
+    public void setValue(EasyTable<V> table, V rowValue, Object value) {
+        
+    }
+
+    public void setCellEditor(TableCellEditor cellEditor) {
+        TableCellEditor old = super.getCellEditor();
+        if (old != null) {
+            old.removeCellEditorListener(this);
+        }
+        super.setCellEditor(cellEditor);
+        cellEditor.addCellEditorListener(this);
+    }
+
+    @Override
+    public void editingStopped(ChangeEvent e) {
+
+    }
+
+    @Override
+    public void editingCanceled(ChangeEvent e) {
+
+    }
+
+    public boolean isEditable() {
+        return isEditable;
+    }
+
+    public void setEditable(boolean isEditable) {
+        this.isEditable = isEditable;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public Class getValueClass() {
+        return valueClass;
+    }
+
+    public void setValueClass(Class valueClass) {
+        this.valueClass = valueClass;
+    }
+
 }
