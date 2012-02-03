dca99949d024901422e68bd93a2ae6cb9d3950c6
hxzon@163.com <hxzon@163.com@d229cd2e-d496-f8b7-c295-5a573b23ac90>
2012-02-03 09:54:21 +0000

    ProtocolIpField.getIpAsString(long theIp)

--- trunk/java-netprotocol/org/hxzon/netprotocol/field/ProtocolIpField.java
+++ trunk/java-netprotocol/org/hxzon/netprotocol/field/ProtocolIpField.java
@@ -1,19 +1,64 @@
 package org.hxzon.netprotocol.field;
 
 import org.hxzon.netprotocol.packet.Packet;
-import org.hxzon.util.BytesUtil;
 
-public class ProtocolIpField extends ProtocolByteArrayField {
+public class ProtocolIpField extends ProtocolInt63Field {
 
     public ProtocolIpField(String name, String display, int offset, int len, Packet srcPacket) {
-        super(name, display, offset, len, srcPacket);
+        super(name, display, offset, len, true, srcPacket);
     }
 
     public String getValueAsString() {
-        String tmp = "";
-        for (int i = 0; i < 4; i++) {
-            tmp += BytesUtil.toUInt(getValue()[i]) + ".";
-        }
-        return tmp;
+        return getIpAsString(getValue());
     }
+
+    public static String getIpAsString(long theIp) {
+        String toReturn = "";
+        for (int i = 0; i < 4; i++) {
+            long field = theIp % 256;
+            theIp = theIp / 256;
+            toReturn = field + toReturn;
+            if (i != 3)
+                toReturn = "." + toReturn;
+        }
+        return toReturn;
+    }
+
+//    public static long IPStringToNum(String theIp) {
+//        String[] fields = stringToArray(theIp, ".");
+//        long sum = 0;
+//        for (int i = 0; i < fields.length; i++) {
+//            sum = sum * 256 + Integer.parseInt(fields[i]);
+//        }
+//        return sum;
+//    }
+//
+//    private static String[] stringToArray(String inString, String delimiter) {
+//        int start = 0, end = 0;
+//        if (inString == null || delimiter == null) {
+//            return null;
+//        }
+//        int count = 1;
+//        int index = 0;
+//        int dl = delimiter.length();
+//        while (true) {
+//            index = inString.indexOf(delimiter, index);
+//            if (index < 0) {
+//                break;
+//            }
+//            count++;
+//            index += dl;
+//        }
+//        String array[] = new String[count];
+//        for (int i = 0; i < count; i++) {
+//            end = inString.indexOf(delimiter, start);
+//            if (end == -1) { // The delimiter wasn't found
+//                array[i] = new String(inString.substring(start)); // extract from start to the end
+//                return array;
+//            }
+//            array[i] = new String(inString.substring(start, end));
+//            start = end + dl; // move the start to the the end of the last delimiter
+//        }
+//        return array;
+//    }
 }
