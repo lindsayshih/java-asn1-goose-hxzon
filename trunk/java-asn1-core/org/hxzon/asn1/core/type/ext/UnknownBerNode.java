37a4d5d9bb96053d085342ea8f9ec9e589f9a7f6
hxzon@163.com <hxzon@163.com@d229cd2e-d496-f8b7-c295-5a573b23ac90>
2012-02-23 06:41:38 +0000

    PdvList when contextValue==3 is MmsPdu

--- trunk/java-asn1-core/org/hxzon/asn1/core/type/ext/UnknownBerNode.java
+++ trunk/java-asn1-core/org/hxzon/asn1/core/type/ext/UnknownBerNode.java
@@ -5,8 +5,21 @@ import org.hxzon.asn1.core.type.BerOctetString;
 
 public class UnknownBerNode extends BerOctetString {
 
+    public UnknownBerNode() {
+        setId("unknown");
+        setName("unknown");
+    }
+
     public UnknownBerNode(int tag) {
         setId("unknown " + Tag.toString(tag));
         setName("unknown " + Tag.toString(tag));
     }
+
+    public void setTag(int tag) {
+        super.setTag(tag);
+        setId("unknown " + Tag.toString(tag));
+        setName("unknown " + Tag.toString(tag));
+        setDisplayString(null);
+    }
+
 }
