6293f502681661d68008e491ba5e2b1963092006
hxzon <hxzon@163.com>
2012-02-29 09:20:50 +0000

    CotpPacketGroup,CotpPacketCache

--- java-netprotocol/org/hxzon/netprotocol/payload/MissPayload.java
+++ java-netprotocol/org/hxzon/netprotocol/payload/MissPayload.java
@@ -1,11 +1,11 @@
 package org.hxzon.netprotocol.payload;
 
-public class NullPayload extends DataPayload {
+public class MissPayload extends DataPayload {
     public String getProtocolTypeDesc() {
         return getSrcPacket().getProtocolTypeDesc();
     }
 
     public String getName() {
-        return "null";
+        return "miss";
     }
 }
