e603b7e0cb5849640ea50f5f2eaece936334491e
hxzon <hxzon@163.com>
2012-05-06 08:27:48 +0000

    1,ProtocolInt31Field,ProtocolIntBigField

--- java-netprotocol/org/hxzon/netprotocol/field/ProtocolInt31HexField.java
+++ java-netprotocol/org/hxzon/netprotocol/field/ProtocolInt31HexField.java
@@ -3,9 +3,9 @@ package org.hxzon.netprotocol.field;
 import org.hxzon.netprotocol.packet.Packet;
 
 public class ProtocolInt31HexField extends ProtocolInt31Field {
-    public ProtocolInt31HexField(String id, String name, int offset, int len, Packet srcPacket) {
-        this(id, name, offset, len, false, srcPacket);
-    }
+//    public ProtocolInt31HexField(String id, String name, int offset, int len, Packet srcPacket) {
+//        this(id, name, offset, len, false, srcPacket);
+//    }
 
     public ProtocolInt31HexField(String id, String name, int offset, int len, boolean unsigned, Packet srcPacket) {
         super(id, name, offset, len, unsigned, srcPacket);
--- java-netprotocol/org/hxzon/netprotocol/field/ProtocolInt63Field.java
+++ java-netprotocol/org/hxzon/netprotocol/field/ProtocolInt63Field.java
@@ -3,9 +3,9 @@ package org.hxzon.netprotocol.field;
 import org.hxzon.netprotocol.packet.Packet;
 
 public class ProtocolInt63Field extends ProtocolField {
-    public ProtocolInt63Field(String id, String name, int offset, int len, Packet srcPacket) {
-        this(id, name, offset, len, false, srcPacket);
-    }
+//    public ProtocolInt63Field(String id, String name, int offset, int len, Packet srcPacket) {
+//        this(id, name, offset, len, false, srcPacket);
+//    }
 
     public ProtocolInt63Field(String id, String name, int offset, int len, boolean unsigned, Packet srcPacket) {
         setPacket(srcPacket);
